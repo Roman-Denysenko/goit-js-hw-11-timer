51bc0ffc7006dee2588e8d5c17dbadd81e186f5b
Roman-Denysenko <roman.denisenko83@gmail.com>
2020-11-08 12:11:50 +0200

    Делает класс таймера

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,39 +1,57 @@
-const refs = {
-    timerContainer: document.querySelector (`#timer-1`),
-    daysContent: document.querySelector(`[data-value="days"]`),
-    hoursContent: document.querySelector(`[data-value="hours"]`),
-    minutesContent: document.querySelector(`[data-value="mins"]`),
-    secondsContent: document.querySelector(`[data-value="secs"]`)
-};
-
 
 class CountdownTimer{
 
-};
-const targetDate = new Date(`Nov 07, 2020`)
-const startTime = targetDate.getTime();
-
-
-setInterval(() => {
-    const currentTime = Date.now();
-     const time = startTime - currentTime;
-    const timer = getTimerComponents(time);
-    updateContent(timer);
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.refs = {
+            timerContainerRef : document.querySelector(`${this.selector}`),
+            daysContent: document.querySelector(`[data-value="days"]`),
+            hoursContent: document.querySelector(`[data-value="hours"]`),
+            minutesContent: document.querySelector(`[data-value="mins"]`),
+            secondsContent: document.querySelector(`[data-value="secs"]`),
+              };
+    };
+     
+    makeTimer() {
+        
+       const startTime = this.targetDate.getTime();
+    setInterval(() => {
+       const currentTime = Date.now();
+       const time = startTime - currentTime;
+       const timer = this.getTimerComponents(time);
+       this.updateContent(timer);
     
-}, 1000)
+    
+    }, 1000);
+   }
+    
 
-function getTimerComponents(time) {
+     getTimerComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
     return ({days, hours, mins, secs})
 
-}
+    }
 
-function updateContent({ days, hours, mins, secs }) {
-        refs.daysContent.textContent = `${days}`;
-        refs.hoursContent.textContent = `${hours}`;
-        refs.minutesContent.textContent = `${mins}`;
-        refs.secondsContent.textContent = `${secs}`;
-}
+     updateContent({ days, hours, mins, secs }) {
+        this.refs.daysContent.textContent = `${days}`;
+        this.refs.hoursContent.textContent = `${hours}`;
+        this.refs.minutesContent.textContent = `${mins}`;
+        this.refs.secondsContent.textContent = `${secs}`;
+    }
+};
+
+
+
+const newTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Nov 17, 2020'),
+});
+
+newTimer.makeTimer();
+
+
+
